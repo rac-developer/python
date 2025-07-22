788fd504e71eb3ad2997ca39d473960b12e4161c
Rodolfo Castro <soyracm@gmail.com>
2025-07-22 12:51:37 -0400

    conversion de datos

---
+++ projects/00-basic/main.py
@@ -0,0 +1,22 @@
+x = 1
+y = 2.0
+z = "3"
+
+print(x) # -> int
+print(y) # -> float
+print(z) # -> str
+
+# Convertir en un numero
+
+# 1era manera
+
+y = int(y)
+z = int(z)
+
+print(y) # -> float
+print(z) # -> str
+
+# 2da manera
+
+print(int(y)) # -> int
+print(int(z)) # -> int

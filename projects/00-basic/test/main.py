5b6264db281aa323fa2234e63b0a900ec1e8b40e
Rodolfo Castro <soyracm@gmail.com>
2025-09-03 11:21:54 -0400

    duck typing

--- projects/00-basic/test/main.py
+++ projects/00-basic/test/main.py
@@ -1,3 +1,28 @@
-# Si la clase es pequeña, lo puedes dejar en tu programa, pero si es muy grande se recomienda ponerlo en un modulo separado
-class Auto:
-    pass
+
+class Pato:
+    def caminar(self):
+        print("El pato esta caminando")
+    def hablar(self):
+        print("El pato esta haciendo cuac cuac")
+        
+class Perro:
+    def caminar(self):
+        print("El perro esta caminando")
+    def hablar(self):
+        print("El perro esta haciendo guau guau")
+
+class Persona:
+    def atrapar(self,pato):
+        pato.caminar()
+        pato.hablar()
+        print("Lo atrape!")
+        
+pato = Pato()
+perro = Perro()
+persona = Persona()
+
+# Al printear la informacion que nos arroja, nos permite saber que animal es
+persona.atrapar(pato)
+# El perro esta caminando
+# El perro esta haciendo guau guau
+# Lo atrape!

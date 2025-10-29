f929df47f583116bb60dac169e2edf18ff9e3110
Rodolfo Castro <soyracm@gmail.com>
2025-10-29 11:16:43 -0400

    moved image

--- projects/00-basic/app/main.py
+++ projects/00-basic/app/main.py
@@ -1,7 +1,4 @@
 from tkinter import *
-# Importamos la siguiente librería para usar temas
-from tkinter.ttk import *
-import time
 
 windows = Tk()
 
@@ -11,32 +8,33 @@ icono = PhotoImage(file='icon.png')
 windows.iconphoto(True, icono)
 windows.config(background='#f2f2f2')
 
-def start():
-    GB = 10
-    dowloaded = 0
-    speed = 1  # GB por segundo
-    while (dowloaded < GB):
-        time.sleep(0.05)
-        # bar['value']+=10
-        # dowloaded+=1
-        bar['value'] += (speed / GB) * 100
-        dowloaded += speed
-        porcentaje.set(str(int((dowloaded/GB)*100)) + "% descargado")
-        texto.set(str(dowloaded) + "/" + str(GB) + " GB descargados")
-        windows.update_idletasks()
+# 1era forma usando python nativo
 
-porcentaje = StringVar()
-porcentaje.set("0% descargado")
+def move_up(event):
+    canvas.move(miImagen, 0, -10)
 
-texto = StringVar()
-texto.set("0/0 GB descargados")
-        
-bar = Progressbar(windows, orient=HORIZONTAL, length=300)
-bar.pack(padx=10, pady=10)
+def move_down(event):
+    canvas.move(miImagen, 0, +10)
 
-textoLabel = Label(windows, textvariable=porcentaje).pack(padx=10, pady=10)
-comLabel = Label(windows, textvariable=texto).pack(padx=10, pady=10)
+def move_left(event):
+    canvas.move(miImagen, -10, 0)
+    
+def move_right(event):
+    canvas.move(miImagen, +10, 0)
 
-boton = Button(windows, text="Descargar", command=start).pack()
+windows.bind("<w>", move_up)
+windows.bind("<s>", move_down)
+windows.bind("<a>", move_left)
+windows.bind("<d>", move_right)
+
+windows.bind("<Up>", move_up)
+windows.bind("<Down>", move_down)
+windows.bind("<Left>", move_left)
+windows.bind("<Right>", move_right)
+
+canvas = Canvas(windows, width=500, height=500)
+canvas.pack()
+
+miImagen = canvas.create_image(0,0,image=icono, anchor=NW)
 
 windows.mainloop()
